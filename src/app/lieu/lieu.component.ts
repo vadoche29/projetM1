3253a563b5f5e8a1d86d34d4b7b352369202ee28
vadoche <vadimmarchand@gmail.com>
2024-01-12 09:57:05 +0100

    site web

---
+++ src/app/lieu/lieu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lieu',
+  standalone: true,
+  imports: [],
+  templateUrl: './lieu.component.html',
+  styleUrl: './lieu.component.scss'
+})
+export class LieuComponent {
+
+}
