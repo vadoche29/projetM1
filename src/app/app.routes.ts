0c401c54b85b1fa9bd1b6f4657c07901df90d25b
vadoche29 <vadim.marchand@isen-ouest.yncrea.fr>
2024-01-31 10:34:38 +0100

    ajout du component visualisation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,12 @@ import { ListComponent } from './list/list.component';
 import { SignalerComponent } from './signaler/signaler.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ListAdminComponent } from './list-admin/list-admin.component';
+import { VisualisationComponent } from './visualisation/visualisation.component';
 
-export const routes: Routes = [{path: 'lieux', component: ListComponent},
-{path:'signaler', component: SignalerComponent},
-{path: '', component: LandingPageComponent},
-{path: 'page-admin', component: ListAdminComponent}];
+export const routes: Routes = [
+	{ path: 'lieux', component: ListComponent },
+	{ path: 'signaler', component: SignalerComponent },
+	{ path: '', component: LandingPageComponent },
+	{ path: 'page-admin', component: ListAdminComponent },
+	{ path: 'visualisation', component: VisualisationComponent }
+];
---
+++ src/app/visualisation/visualisation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualisation',
+  standalone: true,
+  imports: [],
+  templateUrl: './visualisation.component.html',
+  styleUrl: './visualisation.component.scss'
+})
+export class VisualisationComponent {
+
+}
